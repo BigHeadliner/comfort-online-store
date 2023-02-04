181c8bd5a7c64f4205a9b3cee60f89ca6d04cafe
BigHeadliner <bigheadliner@gmail.com>
2023-02-04 22:32:47 +0200

    collection section ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
-$(function () { 
-     
-   
+$(function () {
+
+    var elem = document.querySelector('.collection__items');
+    var iso = new Isotope(elem, {
+        // options
+        itemSelector: '.collection__item',
+        layoutMode: 'fitRows', 
+    });
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,8 @@ function nunjucks(){
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js', 
     'node_modules/jquery-form-styler/dist/jquery.formstyler.js',  
     'node_modules/counterup2/dist/index.js', 
-    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',   
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',    
+    'node_modules/isotope-layout/dist/isotope.pkgd.js',
     'app/js/main.js'
    ]) 
    .pipe(concat('main.min.js')) 

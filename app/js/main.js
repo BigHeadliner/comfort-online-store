463dfe2f96b72940d23ef7637498745c5956867a
BigHeadliner <bigheadliner@gmail.com>
2023-02-05 19:56:43 +0200

    ship section ready

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,13 @@ $(function () {
     var iso = new Isotope(elem, {
         // options
         itemSelector: '.collection__item',
-        layoutMode: 'fitRows', 
+        layoutMode: 'fitRows',
     });
 
+    $('.ship__slider').slick({ 
+        prevArrow:'<button type="button" class="slick-prev"><img src="images/ship/prev.svg" alt=""></button>', 
+        nextArrow:'<button type="button" class="slick-next"><img src="images/ship/next.svg" alt=""></button>', 
+        slidesToShow: 4,
+        slidesToScroll: 1,})
+   
 });
